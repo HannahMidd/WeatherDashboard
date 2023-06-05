28f344f9afebb9ddbf7eb7eed909c77b13ae4839
HannahMidd <hmiddleton1017@gmail.com>
2023-06-04 22:55:24 -0400

    adding JS content

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,18 @@
-fetch("https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={01c2f043e94a1460b1f6d351b9f98ce6}")
+// Grab latitude, longitude and app ID from city user entered:
+const lat = ;
+const lon = ;
+cons appid = "";
+
+// --------------------- Fetch that data from API------------------------------------------
+
+fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${appid}`)
+
 .then (function(response) {
-  return response.json():
+  return response.json();
 })
+
 .then (function(data) {
-  console.log(We have weather data!)
+  console.log(We have weather data!, data);
 });
+
 
