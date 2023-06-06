78f810318d8c8e72591335faeb1619e804c473f3
HannahMidd <hmiddleton1017@gmail.com>
2023-06-05 23:02:18 -0400

    adding API key and JS search history

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,38 @@
-// Grab latitude, longitude and app ID from city user entered:
-let lat;
-let lon;
-let appid;
+//------------------------ Global Variables to be used ---------------------
+const weatherApiKey = "01c2f043e94a1460b1f6d351b9f98ce6";
+const weatherMainURL = "https://api.openweathermap.org";
+const searchForm = document.querySelector("#search-form");
+const searchInput = document.querySelector("#search-input");
+const todayContainer = document.querySelector("#today");
+const forecastContainer = document.querySelector("#forecast");
+const searchHistoryContainer = document.querySelector("#history");
 
-// --------------------- Fetch that data from API------------------------------------------
+// --------------------- Fetch weather data from API------------------------------------------
 
-fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${appid}`)
+fetch(
+  "https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=01c2f043e94a1460b1f6d351b9f98ce6"
+)
+  .then(function (response) {
+    return response.json();
+  })
 
-.then (function(response) {
-  return response.json();
-})
+  .then(function (data) {
+    console.log("We have weather data!", data);
+  });
 
-.then (function(data) {
-  console.log("We have weather data!", data);
-});
+// Show previous searches for easy user access
 
+function renderSearchHistory() {
+  searchHistoryContainer.innerHTML = "";
 
+  for (var i = searchHistory.length - 1; i >= 0; i--) {
+    var btn = document.createElement("button");
+    btn.setAttribute("type", "button");
+    btn.setAttribute("aria-controls", "today forecast");
+    btn.classList.add("history-btn", "btn-history");
+
+    btn.setAttribute("data-search", searchHistory[i]);
+    btn.textContent = searchHistory[i];
+    searchHistoryContainer.append(btn);
+  }
+}
